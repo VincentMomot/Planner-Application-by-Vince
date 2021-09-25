172479d38a4393e42a7fdca397fbc34ade44d4f5
Vince Momot <vincemomot@gmail.com>
2021-09-25 19:29:55 -0400

    finished the code, need readME finished

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Planner-Application-by-Vince
+Use this planner in your daily life to keep track of your tasks.
+
+### Website Features:
+1) 
+
+
+### What I learned from the creation of this site:
+* 
+
+Try your hand at the Quiz at [Vincent D Momot's JS Quiz](https://vincentmomot.github.io/Vince-s-JavaScript-Quiz/index)
+
+Screenshots of the site:
+![This is an image of my main page](assets/ss.png)
--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,12 @@ var formEL = document.querySelector(".form-control");
 dateEL.textContent = moment().format('[Today is: ] dddd[,] MMMM Do');
 
 //gives the time in hour integers
-var now =moment().format('H');
+var now = moment().format('H');
 var color = [a, b, c, d, e, f, g, h, i];
 
 //colors current hour RED
 if (now < color.length - 1) { }
-else if (now > color.length - 1 && now < color.length + 8) {
+else if (now > color.length-1 && now < color.length + 9) {
     var red = color[now - color.length];
     red.style.background = "red";
 }
@@ -33,31 +33,45 @@ else if (now>9) {
 
 
 
-var save;
-localStorage.getItem("save");
-var btnEL=$(".btn");
+
+var save = localStorage.getItem("save");
+var btnEL=$(".btn");    
 btnEL.on("click", saveFunction);
-
-
-//if there is saved data, retieve it
-if(save==1){
-taskText=JSON.parse(localStorage.getItem("localTasks"));
-
+if(save!=1){
+var tasks=["","","","","","","","",""];
+localTasks=localStorage.setItem("localTasks",JSON.stringify(tasks)); //this
 }
+//if there is saved data, retieve it
+else{
+var tasks=JSON.parse(localStorage.getItem("localTasks"));
+//old saves will be displayed here
+for(var k=0; k<tasks.length; k++){
+//console.log(color[k])
+color[k].value=tasks[k];
+//console.log(color[k].value) 
+}}
 
 
-//when saved button is clicked
+
+//when saved button is clicked it will write to dataSave
 function saveFunction(){
-    // console.log($(this.prev));
-    var hourInput=$(this).prev().val()
-    console.log(hourInput);
+    var hourInput=$(this).prev().val(); //this is the text in the box
+    //console.log(hourInput);
+    var saveSpot=($(this).prev().attr('id')); //this is the id of the textbox
     
+    for(var l=0; l<color.length; l++){
 
+        if ($(color[l]).attr('id')==saveSpot){
+            tasks[l]=hourInput; 
+            localTasks=localStorage.setItem("localTasks",JSON.stringify(tasks)); //this
+        }
+    }
 
     save=1;
     localStorage.setItem("save",save);
-
 }
+
+
 
 
 
